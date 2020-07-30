f50baec7b4892e1f3cf8fe3735c138738347417c
Felix Vemmer <felix.vemmer@gmail.com>
2020-07-30 19:24:46 +0200

    experimenting with design

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.vemmerlegaltech',
-      version='0.0.10',
+      version='0.0.11',
       description=('A docassemble extension.'),
       long_description='To be written',
       long_description_content_type='text/markdown',
